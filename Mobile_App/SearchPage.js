78ed621794f2eda2c4a08b98dbb3289aab9a6e78
CUB3D <callumthom11@gmail.com>
2022-02-10 14:05:00 +0000

    Tested on android, minor fixes

--- Mobile_App/SearchPage.js
+++ Mobile_App/SearchPage.js
@@ -4,39 +4,46 @@ import axios from 'axios';
 import {ListItem, TextInput} from "@react-native-material/core";
 
 export const SearchPage = ({ navigation }) => {
-    const [text, onChangeText] = useState("query");
+    const [text, onChangeText] = useState("");
     const [results, onChangeResults] = useState([]);
 
     return (
        <View style={styles.container}>
-           <TextInput placeholder="search" variant="standard" onChangeText={(new_text) => {
+           <View style={styles.innerContainer}>
+               <TextInput placeholder="search" variant="standard" onChangeText={(new_text) => {
 
-               if(new_text.length === 0) {
-                   onChangeResults([]);
-               } else {
-                   axios.get("http://www.uniquechange.com/fwApp/api/search.php?l=5&q=" + new_text, { withCredentials: true })
-                       .then(response => {
-                           onChangeResults(response.data);
-                       })
-                       .catch(error => {
-                           console.log(error);
-                       });
-               }
+                   if(new_text.length === 0) {
+                       onChangeResults([]);
+                   } else {
+                       axios.get("http://www.uniquechange.com/fwApp/api/search.php?l=5&q=" + new_text, { withCredentials: true })
+                           .then(response => {
+                               onChangeResults(response.data);
+                           })
+                           .catch(error => {
+                               console.log(error);
+                           });
+                   }
 
-               onChangeText(new_text);
-           }} value={text}/>
+                   onChangeText(new_text);
+               }} value={text}/>
 
-           <>
-            { results.map((v) => <ListItem title={v.ID + " | " + v.title} key={v.ID} onPress={() => {
-                navigation.navigate("questions", {questionId: v.ID});
-            }}/>)}
-           </>
+               <>
+                { results.map((v) => <ListItem title={v.ID + " | " + v.title} key={v.ID} onPress={() => {
+                    navigation.navigate("questions", {questionId: v.ID});
+                }}/>)}
+               </>
+           </View>
        </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+    },
+    innerContainer: {
         flex: 1,
         backgroundColor: '#fff',
         marginLeft: 16,
