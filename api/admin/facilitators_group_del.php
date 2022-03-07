ccb9f0337ea5c527d7af3bb976d0d8491531d40e
CUB3D <callumthom11@gmail.com>
2022-03-07 02:47:58 +0000

    Backend Sentry

--- api/admin/facilitators_group_del.php
+++ api/admin/facilitators_group_del.php
@@ -1,22 +1,7 @@
 <?php
-/// Handler for retrieving a list of all phases
+/// Handler for deleting a group
 
-include_once '../../config/Database.php';
-
-// If using the token override (mobile app when running in browser)
-if (array_key_exists("HTTP_X_AUTH_TOKEN", $_SERVER ?? [])) {
-    session_id($_SERVER["HTTP_X_AUTH_TOKEN"]);
-}
-
-session_start();
-
-// Set cors header
-header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X-HTTP-Method-Override, X-Auth-Token");
-header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,UPDATE,OPTIONS");
-header('Access-Control-Allow-Credentials: true');
-header('Content-Type: application/json');
-
+require_once __DIR__ . '/../api_common.php';
 
 if($_ENV["AUTH_DISABLE"] !== true) {
     // Ensure that the requester is actually authenticated
